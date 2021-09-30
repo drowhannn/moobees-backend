fce881a7b692da8df101cd122cacd841fc24e04b
Rohan Dhimal <drowhann@gmail.com>
2021-09-30 22:22:10 +0545

    first commit

---
+++ moviesDatabase.js
@@ -0,0 +1,74 @@
+import mongoose from "mongoose";
+
+const movieSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  posterUrl: {
+    type: String,
+    required: true,
+  },
+  bannerUrl: {
+    type: String,
+    required: true,
+  },
+  releaseDate: {
+    type: Date,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  ageRating: {
+    type: String,
+    required: true,
+  },
+  genres: {
+    type: [String],
+    required: true,
+  },
+  runTime: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: [String],
+    required: true,
+  },
+  casts: {
+    type: [String],
+    required: true,
+  },
+  keywords: {
+    type: [String],
+    required: true,
+  },
+  revenue: {
+    type: Number,
+    required: true,
+  },
+  budget: {
+    type: Number,
+    required: true,
+  },
+  originalLanguage: {
+    type: String,
+    required: true,
+  },
+  updated: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("movie", movieSchema);
---
+++ server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import mongoose from "mongoose";
+import Movies from "./moviesDatabase.js";
+
+//app config
+const app = express();
+const port = process.env.PORT || 9000;
+
+//middleware
+app.use(express.json());
+
+//db config
+const connectionUrl =
+  "mongodb+srv://admin:PktB9grK2L3Szj5@cluster0.ipnq5.mongodb.net/moviesdb?retryWrites=true&w=majority";
+
+mongoose.connect(connectionUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//???
+
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("Database is connected successfully.");
+});
+
+//api routes
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.post("/movies/new", (req, res) => {
+  const movieData = req.body;
+  Movies.create(movieData, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(`New movie created successfully:\n${data}`);
+    }
+  });
+});
+
+//listen
+app.listen(port, () => {
+  console.log(`Listening on localhost:${port}`);
+});
